f072a7cd0532e350259d49683f0b84a010187c3e
fabian <fabianarj467@gmail.com>
2025-12-03 15:39:28 -0400

    cambios para correr en render

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -70,19 +70,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 
-if os.environ.get("RENDER"):    
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600
-        )
-    }
+DATABASES = {}
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        env="DATABASE_URL",
+        conn_max_age=600,
+        ssl_require=False,  
+    )
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
-        }
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 
 AUTH_PASSWORD_VALIDATORS = [
